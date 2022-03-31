e3a93da4bad4b60b5a0646be9840806fd076bf6f
ma9da <dodymasry25@gmail.com>
2022-04-01 01:58:13 +0200

    added: services component

---
+++ src/components/services/Services.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+function Services() {
+    return (
+        <section class="service">
+            <div class="container flex-container">
+                <h2 class="title">our services</h2>
+                <div class="servicesCards">
+                    {/* <!-- card start --> */}
+                    <div class="servicesCard">
+                        <i class="fa-duotone fa-pen-ruler"></i>
+                        <h3>UI/UX DESIGN</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione adipisci mollitia quis
+                            necstatibus.
+                        </p>
+                    </div>
+                    {/* <!-- card end --> */}
+                    {/* <!-- card start --> */}
+                    <div class="servicesCard">
+                        <i class="fa-solid fa-laptop-code"></i>
+                        <h3>APP DEVELOPMENT</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione adipisci mollitia quis
+                            necstatibus.
+                        </p>
+                    </div>
+                    {/* <!-- card end --> */}
+                    {/* <!-- card start --> */}
+                    <div class="servicesCard">
+                        <i class="fa-light fa-tachograph-digital"></i>
+                        <h3>PRODUCT DESIGN</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione adipisci mollitia quis
+                            necstatibus.
+                        </p>
+                    </div>
+                    {/* <!-- card end --> */}
+                    {/* <!-- card start --> */}
+                    <div class="servicesCard">
+                        <i class="fa-light fa-gears"></i>
+                        <h3>UI/UX DESIGN</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione adipisci mollitia quis
+                            necstatibus.
+                        </p>
+                    </div>
+                    {/* <!-- card end --> */}
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Services
