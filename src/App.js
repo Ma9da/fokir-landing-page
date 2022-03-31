1289e69506f368742df5228e3b04e68626a09850
ma9da <dodymasry25@gmail.com>
2022-04-01 01:59:13 +0200

    added: services to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Hello from "./components/hello/Hello";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
+import Services from "./components/services/Services";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
     <Navbar/>
     <Hero/>
     <Hello/>
+    <Services/>
     </>
   );
 }
