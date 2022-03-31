878d44a2446ef62a6fcb91764792c446cffad60e
ma9da <dodymasry25@gmail.com>
2022-04-01 01:51:49 +0200

    edit: class to className

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import "./hero.css"
 function Hero() {
     return (
-        <section class="hero paddingY">
-            <div class="conatiner flex-container">
-                <header class="heroHeader">
+        <section className="hero paddingY">
+            <div className="conatiner flex-container">
+                <header className="heroHeader">
                     <p>Hello i'm</p>
-                    <h1 class="heroTitle">ALEX SMITH</h1>
+                    <h1 className="heroTitle">ALEX SMITH</h1>
                     <p>professional web
-                        <span class="txt-rotate" data-period="500" data-rotate='[ "Developer", "Designer"]'></span>
+                        <span className="txt-rotate" data-period="500" data-rotate='[ "Developer", "Designer"]'></span>
                     </p>
                 </header>
             </div>
