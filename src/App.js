46e2913e91a2fa3384c8d2916321ea5b3fd1d6ef
ma9da <dodymasry25@gmail.com>
2022-04-01 02:08:33 +0200

    added: numbers to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Hello from "./components/hello/Hello";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
+import Numbers from "./components/numbers/Numbers";
 import Portfolio from "./components/portfolio/Portfolio";
 import Services from "./components/services/Services";
 
@@ -12,6 +13,7 @@ function App() {
     <Hello/>
     <Services/>
     <Portfolio/>
+    <Numbers/>
     </>
   );
 }
