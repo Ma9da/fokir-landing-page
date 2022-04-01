141b3350a0393d3886b62cb3e91bfcacdc0c9ae1
ma9da <dodymasry25@gmail.com>
2022-04-01 02:26:07 +0200

    added: footer component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Blog from "./components/blog/Blog";
 import Client from "./components/client/Client";
+import Contact from "./components/contact/Contact";
 import Hello from "./components/hello/Hello";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
@@ -18,6 +19,7 @@ function App() {
     <Numbers/>
     <Client/>
     <Blog/>
+    <Contact/>
     </>
   );
 }
